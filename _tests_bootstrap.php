9cb0d8e6bb4873e76fc7b6d6454cce940c4aba99
Jaroslav Tyc <jaroslav.tyc@expensa.cz>
2015-01-28 14:18:27 +0100

    Tests check if native SPL types are disabled

--- _tests_bootstrap.php
+++ _tests_bootstrap.php
@@ -1,2 +1,6 @@
 <?php
+if (extension_loaded('spl_types')) {
+    throw new \LogicException('PHP extension spl_types is loaded. Tests of this fallback library can not be tested.');
+}
+
 require_once 'vendor/autoload.php';
